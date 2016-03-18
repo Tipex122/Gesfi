d162ba9416114ef5fa2ac4483044068e66ec3098
Tipex122 <xavierxlh@yahoo.fr>
2016-03-18 22:46:10 +0100

    Added categories App and consequently adjusted gesfi settings

---
+++ categories/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from decimal import Decimal
+from mptt.models import MPTTModel, TreeForeignKey
+
+# Create your models here.
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=100, blank=False, unique=True)
+    description = models.TextField(blank=True, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal('0.00'), verbose_name="Estimated budget", blank=True, null=True)
+
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    class Meta:
+        verbose_name_plural = u"Categories"
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.is_root_node():
+            super(Category, self).save(*args, **kwargs) # Call the "real" save() method.
+            ancestors = self.get_ancestors(True)
+            level_amount=0
+            for sibling in self.get_siblings(True):
+                level_amount = level_amount+sibling.amount
+            for ancestor in ancestors:
+                ancestor.amount=level_amount
+                ancestor.save()
+                for sibling in ancestor.get_siblings():
+                    level_amount = level_amount+sibling.amount
+        super(Category, self).save(*args, **kwargs) # Call the "real" save() method.
+
--- gesfi/settings.py
+++ gesfi/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'categories',
     'gunicorn',
+    'mptt',
+    'django_mptt_admin',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -56,7 +59,7 @@ ROOT_URLCONF = 'gesfi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,9 +108,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
-TIME_ZONE = 'UTC'
+#TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
+
 
 USE_I18N = True
 
@@ -120,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
