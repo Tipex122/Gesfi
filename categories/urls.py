3bd0ab67926606dd9b8dca1e72709d979d5d8170
Tipex122 <xavierxlh@yahoo.fr>
2016-04-18 22:32:32 +0200

    Addes Json serializer for test

--- categories/urls.py
+++ categories/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
 #    url(r'^$', 'categories.views.category_list', name='category_list'),
     url(r'^budget/$', views.category_list),
+    url(r'^test/$', views.category_json, name='categories_json'),
 ]
--- categories/views.py
+++ categories/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, redirect, render
+from django.http import HttpResponse
+from django.core import serializers
 
 from .models import Category
 
@@ -10,3 +12,7 @@ def category_list(request):
     categories = Category.objects.all()
     context = {'categories': categories}
     return render(request, 'Categories/categories_list.html', context)
+
+def category_json(request):
+    categories = Category.objects.all()
+    return HttpResponse(serializers.serialize('json', categories), content_type="application/json")
