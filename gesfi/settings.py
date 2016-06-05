cf37a5f09711f75b3183d705447a8a3cd1dc2929
Pi RASPOUTINE <xavierxlh@yahoo.fr>
2016-06-05 22:14:27 +0200

    Modification configuration

--- gesfi/settings.py
+++ gesfi/settings.py
@@ -132,7 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'collect_static')
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'collect_static/')
+
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
