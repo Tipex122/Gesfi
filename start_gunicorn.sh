d56ae2732449c83d99432fff501cb89bc6e6b5a7
Pi RASPOUTINE <xavierxlh@yahoo.fr>
2016-06-05 22:05:29 +0200

    Ajustement configuration démarrage gunicorn

--- start_gunicorn.sh
+++ start_gunicorn.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 cd /home/pi/Prog/django/gesfi
 source ../venv_gesfi/bin/activate
-gunicorn gesfi.wsgi:application --name "gesfinancière" \
+gunicorn gesfi.wsgi:application --name "gesfi" \
 	--workers=2 \
+	--timeout=120 \
 	--bind=192.168.3.13:8000 \
-	--user="www-data" \
-	--group="www-data"
+	--user="pi" \
+	--group="pi"
 	
