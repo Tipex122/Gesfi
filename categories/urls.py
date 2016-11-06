940eefc50d0c42e44a7bffd4acccc78d9da29861
Tipex122 <xavierxlh@yahoo.fr>
2016-11-06 20:33:22 +0100

    Ajusted global behaviour (login/logout + different css files)

--- categories/urls.py
+++ categories/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
 #    url(r'^$', 'categories.views.category_list', name='category_list'),
-    url(r'^budget/$', views.category_list),
+    url(r'^budget/$', views.category_list, name='budget'),
     url(r'^test/$', views.category_json, name='categories_json'),
 ]
--- managegesfi/urls.py
+++ managegesfi/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, name='gesfi_login'),
 
 #    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
-    url(r'^logout/$', auth_views.logout, {'next_page': '/'}),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
 
     url(r'^search/$', search_views, name='search'),
 
