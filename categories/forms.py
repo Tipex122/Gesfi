094e385f2e9f7b98deba898e2b0c09dbebb6325c
Tipex122 <xavierxlh@yahoo.fr>
2016-12-27 19:42:41 +0100

    Starting to manage link between Transactions, Categories and Tags

--- categories/forms.py
+++ categories/forms.py
@@ -1,9 +1,15 @@
 from django import forms
+from mptt.forms import TreeNodeChoiceField
 
 from .models import Tag
+from categories.models import Category
 
 class TagForm(forms.ModelForm):
 
     class Meta:
         model = Tag
         fields = ('tag', 'is_new_tag', 'will_be_used_as_tag', 'category',)
+#        widgets = {
+#            'category':TreeNodeChoiceField(queryset=Category.objects.all()),
+#        }
+    category = TreeNodeChoiceField(queryset=Category.objects.all(), level_indicator=u'+-')
